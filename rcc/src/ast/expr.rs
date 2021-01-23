31209f48ecbe6e8b5ac8fc0c53b932efd800a8ed
jiang <392711804@qq.com>
2021-01-23 18:21:27 +0800

    add expr enum variants

--- rcc/src/ast/expr.rs
+++ rcc/src/ast/expr.rs
@@ -1,8 +1,9 @@
 use std::fmt;
 use std::fmt::{Debug, Formatter, Write};
+use strenum::StrEnum;
 
+use crate::lexer::token::Token;
 use crate::lexer::token::Token::{Minus, Not, Star};
-use crate::lexer::token::{Token};
 
 #[derive(Debug, PartialEq)]
 pub enum Expr {
@@ -10,6 +11,23 @@ pub enum Expr {
     Lit(LitExpr),
     Unary(UnAryExpr),
     Block(BlockExpr),
+    Borrow(BorrowExpr),
+    BinOp(BinOpExpr),
+    Group(GroupExpr),
+    Array(ArrayExpr),
+    ArrayIndex(ArrayIndexExpr),
+    Tuple(TupleExpr),
+    TupleIndex(TupleIndexExpr),
+    Struct(StructExpr),
+    EnumVariant,
+    Call,
+    MethodCall,
+    FieldAccess,
+    Loop,
+    Range,
+    If,
+    Match,
+    Return,
     Nothing,
 }
 
@@ -62,7 +80,7 @@ impl Debug for UnOp {
 }
 
 impl UnOp {
-    pub(crate) fn from_token(tk: &Token) -> Option<Self> {
+    pub fn from_token(tk: &Token) -> Option<Self> {
         match tk {
             Minus => Some(Self::Neg),
             Star => Some(Self::Deref),
@@ -71,3 +89,146 @@ impl UnOp {
         }
     }
 }
+
+#[derive(Debug, PartialEq)]
+struct BorrowExpr {
+    borrow_cnt: u32,
+    is_mut: bool,
+    expr: Box<Expr>,
+}
+
+#[derive(Debug, PartialEq)]
+struct BinOpExpr {
+    lhs: Box<Expr>,
+    bin_op: BinOp,
+    rhs: Box<Expr>,
+}
+
+#[derive(StrEnum, Debug, PartialEq)]
+enum BinOp {
+    /// Arithmetic or logical operators
+    #[strenum("+")]
+    Plus,
+
+    #[strenum("-")]
+    Minus,
+
+    #[strenum("*")]
+    Star,
+
+    #[strenum("/")]
+    Slash,
+
+    #[strenum("%")]
+    Percent,
+
+    #[strenum("^")]
+    Caret,
+
+    #[strenum("&")]
+    And,
+
+    #[strenum("|")]
+    Or,
+
+    #[strenum("<<")]
+    Shl,
+
+    #[strenum(">>")]
+    Shr,
+
+    /// Lazy boolean operators
+    #[strenum("&&")]
+    AndAnd,
+
+    #[strenum("||")]
+    OrOr,
+
+    /// Type cast operator
+    As,
+
+    /// Compound assignment operators
+    #[strenum("+=")]
+    PlusEq,
+
+    #[strenum("-=")]
+    MinusEq,
+
+    #[strenum("*=")]
+    StarEq,
+
+    #[strenum("/=")]
+    SlashEq,
+
+    #[strenum("%=")]
+    PercentEq,
+
+    #[strenum("^=")]
+    CaretEq,
+
+    #[strenum("&=")]
+    AndEq,
+
+    #[strenum("|=")]
+    OrEq,
+
+    #[strenum("<<=")]
+    ShlEq,
+
+    #[strenum(">>=")]
+    ShrEq,
+
+    /// Assignment operators
+    #[strenum("=")]
+    Eq,
+
+    /// Comparison operators
+    #[strenum("==")]
+    EqEq,
+
+    #[strenum("!=")]
+    Ne,
+
+    #[strenum(">")]
+    Gt,
+
+    #[strenum("<")]
+    Lt,
+
+    #[strenum(">=")]
+    Ge,
+
+    #[strenum("<=")]
+    Le,
+
+    /// Range inclusive operators
+    #[strenum("..=")]
+    DotDotEq,
+}
+
+/// GroupExpr -> `(` Expr `)`
+#[derive(Debug, PartialEq)]
+struct GroupExpr(Box<Expr>);
+
+#[derive(Debug, PartialEq)]
+struct ArrayExpr {
+    // TODO
+}
+
+#[derive(Debug, PartialEq)]
+struct ArrayIndexExpr {
+    // TODO
+}
+
+#[derive(Debug, PartialEq)]
+struct TupleExpr {
+    // TODO
+}
+
+#[derive(Debug, PartialEq)]
+struct TupleIndexExpr {
+    // TODO
+}
+
+#[derive(Debug, PartialEq)]
+struct StructExpr;
